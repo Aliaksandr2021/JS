57ebb66dd3584c8effdc6af6d69e5f2164537ca0
Aliaksandr Baranouski <attack18@tut.by>
2022-05-16 20:04:26 +0300

    task 2

--- Block3.js
+++ Block3.js
@@ -7,3 +7,14 @@ console.log(b);
 let c = 21 + true;
 
 console.log(c);
+
+
+let d = 'string' * true;
+console.log(d);
+
+let e = 'string' * 21;
+console.log(e);
+
+let f = 21 * true;
+
+console.log(f);
